56a7eb717a8d9ecda5c53e0a598eb34893bae2b0
Krishna Kalyan <krishnakalyan3@gmail.com>
2019-05-07 21:56:17 +0200

    more update

--- deep-learning/quick-setup.sh
+++ deep-learning/quick-setup.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
+export IP=""
+
+sudo apt-get -y update
+sudo apt-get install nfs-common
+
 conda install -c pytorch -c fastai fastai
+python3 -c "import fastai.utils.collect_env; fastai.utils.collect_env.show_install(1)"
+
+sudo mkdir gcp-mount
+sudo mount 10.14.15.82:/r8_images gcp-mount
