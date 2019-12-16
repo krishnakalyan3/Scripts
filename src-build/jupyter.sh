92e470e7046ebdc26ebd6542c1a91ebcfce2ec63
Ubuntu <ubuntu@ip-172-31-37-245.eu-central-1.compute.internal>
2019-12-16 22:01:22 +0000

    jupyter lab config

---
+++ src-build/jupyter.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+conda install -c conda-forge jupyterlab
+sudo apt install npm
+pip install ipywidgets bokeh pynvml jupyterlab-nvdashboard
+
+# Setup Jupyter
+jupyter notebook --generate-config
+echo "c.NotebookApp.allow_origin = '*'" >> ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.ip = '0.0.0.0'" >> ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.open_browser = False" >> ~/.jupyter/jupyter_notebook_config.py
+
+# Choose password
+jupyter notebook password
+jupyter nbextension enable --py widgetsnbextension --sys-prefix
+
