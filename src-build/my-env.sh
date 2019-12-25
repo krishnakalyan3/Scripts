b3295f431b19306c22a845dd44385b546b3ca5a6
krishnakalyan3 <skalyan@cloudera.com>
2019-12-25 20:27:08 +0100

    build

--- src-build/my-env.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-chmod +x Miniconda3-latest-Linux-x86_64.sh
-./Miniconda3-latest-Linux-x86_64.sh -b
-echo 'export PATH=/root/miniconda3/bin:$PATH' >> ~/.bashrc
-source .bashrc
-
-conda install -c pytorch -c fastai fastai
--- src-build/swift.sh
+++ src-build/swift.sh
@@ -3,6 +3,7 @@
 sudo apt-get install clang libpython-dev libblocksruntime-dev
 wget https://storage.googleapis.com/swift-tensorflow-artifacts/releases/v0.6/rc2/deduped/swift-tensorflow-RELEASE-0.6-cuda10.1-cudnn7-ubuntu18.04.tar.gz
 tar -xvzf swift-tensorflow-RELEASE-0.6-cuda10.1-cudnn7-ubuntu18.04.tar.gz swift
+mkdir swift
 export PATH=$PATH:/home/ubuntu/swift/usr/bin
 
 git clone https://github.com/google/swift-jupyter
