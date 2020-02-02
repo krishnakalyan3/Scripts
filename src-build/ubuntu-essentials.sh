363c6b8c209e037e916092f437d18e57a121297a
Krishna Kalyan <kkalyan@apache.org>
2020-02-02 19:51:34 +0100

    Update and rename ubuntu-essentials to ubuntu-essentials.sh

---
+++ src-build/ubuntu-essentials.sh
@@ -0,0 +1,19 @@
+sudo apt-get update && sudo apt-get --assume-yes upgrade
+sudo apt-get  --assume-yes install screen build-essential gcc g++ make binutils python3-pip
+sudo apt-get --assume-yes install software-properties-common vim screen htop
+
+# Cuda
+wget https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64
+sudo dpkg -i cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64
+sudo apt-key add /var/cuda-repo-10-0-local-10.0.130-410.48/7fa2af80.pub
+sudo apt-get update
+sudo apt-get install cuda
+nvidia-smi
+
+# Nvtop
+git clone https://github.com/Syllo/nvtop.git
+mkdir -p nvtop/build && cd nvtop/build
+cmake ..
+make
+make install
+nvtop
