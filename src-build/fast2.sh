6d50afd4e28b927042efccfa2e33c101cc7ea70d
Krishna Kalyan <kkalyan@apache.org>
2020-02-15 12:20:51 +0100

    Update fast2.sh

--- src-build/fast2.sh
+++ src-build/fast2.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-conda install -y -c fastai -c pytorch fastai
 git clone https://github.com/fastai/fastai2
 cd fastai2
 conda env create -f environment.yml
 source activate fastai2
-make fastai2
+pip install -e ".[dev]"
