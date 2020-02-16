caa1e78e35f8618ac1128645b8e9947bd26b3832
Krishna Kalyan <krishnakalyan3@gmail.com>
2020-02-16 12:51:44 +0100

    Update anaconda.sh

--- src-build/anaconda.sh
+++ src-build/anaconda.sh
@@ -4,7 +4,7 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh -b
-echo 'export PATH=/root/miniconda3/bin:$PATH' >> ~/.bashrc
+echo 'export PATH=/home/ubuntu/miniconda3/bin:$PATH' >> ~/.bashrc
 
 # More libraries
 # conda install anaconda -y 
