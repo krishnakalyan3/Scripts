1f2b8e5217101000b461ed68c961858f6b8a3a21
Krishna Kalyan <krishnakalyan3@gmail.com>
2020-03-03 19:27:44 -0800

    Update vss-code.sh

--- src-build/vss-code.sh
+++ src-build/vss-code.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget https://github.com/cdr/code-server/releases/download/2.1692-vsc1.39.2/code-server2.1692-vsc1.39.2-linux-x86_64.tar.gz
+wget https://github.com/cdr/code-server/releases/download/2.1698/code-server2.1698-vsc1.41.1-linux-x86_64.tar.gz
 tar -xvzf code-server2.1692-vsc1.39.2-linux-x86_64.tar.gz
 cd code-server2.1692-vsc1.39.2-linux-x86_64
 ./code-server  --host 0.0.0.0
