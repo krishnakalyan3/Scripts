543cfff9426e712550bc45de817e729bcd440217
Krishna Kalyan <krishnakalyan3@gmail.com>
2020-03-08 18:08:29 -0700

    Update vss-code.sh

--- src-build/vss-code.sh
+++ src-build/vss-code.sh
@@ -4,3 +4,5 @@ wget https://github.com/cdr/code-server/releases/download/2.1698/code-server2.16
 tar -xvzf code-server2.1698-vsc1.41.1-linux-x86_64.tar.gz
 cd code-server2.1698-vsc1.41.1-linux-x86_64
 ./code-server  --host 0.0.0.0
+
+@category:"programming languages" @sort:installs 
