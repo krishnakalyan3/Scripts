698d82a8d55b4c335a85d12e8f0940c1fee506ba
Krishna Kalyan <kkalyan@apache.org>
2020-09-20 12:59:34 +0200

    Update vss-code.sh

--- src-build/vss-code.sh
+++ src-build/vss-code.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget https://github.com/cdr/code-server/releases/download/3.4.1/code-server-3.4.1-linux-x86_64.tar.gz
+wget https://github.com/cdr/code-server/releases/download/v3.5.0/code-server-3.5.0-linux-x86_64.tar.gz
 tar -xvzf code-server-3.4.1-linux-x86_64.tar.gz
 cd code-server-3.4.1-linux-x86_64
 export PASSWORD="abcd1234"
