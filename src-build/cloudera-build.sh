62a902d8c1c74af5db70b2c474eaa4d24a78bcc6
Krishna Kalyan <krishnakalyan3@gmail.com>
2021-10-09 22:45:13 +0200

    Update and rename anaconda.sh to cloudera-build.sh

---
+++ src-build/cloudera-build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Anaconda Unix 3.7
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh -b
+echo export PATH=$PATH:$(pwd)/miniconda3/bin  >> ~/.bashrc
+source .bashrc
+
+# Install Jupyter Lab
+conda install -c conda-forge jupyterlab -y
+jupyter notebook --generate-config
+echo "c.NotebookApp.allow_origin = '*'" >> ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.ip = '0.0.0.0'" >> ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.open_browser = False" >> ~/.jupyter/jupyter_notebook_config.py
+
+# Passwd
+jupyter notebook password
+
+# 
+
+# More libraries
+# conda install anaconda -y 
+# conda install conda
+# conda install -c pytorch -c fastai fastai
