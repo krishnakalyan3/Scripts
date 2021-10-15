056619f3a2dbd6cca8c2430866a48dfafd99c1b3
Krishna Kalyan <krishnakalyan3@gmail.com>
2021-10-15 17:03:02 +0200

    Update gpu-check.py

--- deep-learning/gpu-check.py
+++ deep-learning/gpu-check.py
@@ -3,17 +3,19 @@
 from tensorflow.python.client import device_lib
 print(device_lib.list_local_devices())
 
+import tensorflow as tf
+tf.test.is_gpu_available()
+
 from keras import backend as K
 K.tensorflow_backend._get_available_gpus()
 
 import torch
 torch.cuda.is_available()
-print(torch.__version__)
 
-import fastai.utils.collect_env; fastai.utils.collect_env.show_install(1)
+print(torch.__version__)
 
 # Check if Pytorch Works
 device = torch.device("cuda0")
 torch.zeros(4,3, device=device)
 
-
+import fastai.utils.collect_env; fastai.utils.collect_env.show_install(1)
