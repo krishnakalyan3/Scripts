9bef5deb49e5d35b513b769faa74892c600a693b
Krishna Kalyan <krishnakalyan3@gmail.com>
2021-10-15 14:28:09 +0200

    Update nvidia-setup.sh

--- src-build/nvidia-setup.sh
+++ src-build/nvidia-setup.sh
@@ -1,1 +1,25 @@
+# https://medium.com/analytics-vidhya/install-cuda-11-2-cudnn-8-1-0-and-python-3-9-on-rtx3090-for-deep-learning-fcf96c95f7a1
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y build-essential cmake unzip pkg-config
+sudo apt-get install -y libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev
+sudo apt-get install -y libjpeg-dev libpng-dev libtiff-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install -y libxvidcore-dev libx264-dev
+sudo apt-get install -y libgtk-3-dev
+sudo apt-get install -y libopenblas-dev libatlas-base-dev liblapack-dev gfortran
+sudo apt-get install -y libhdf5-serial-dev graphviz
+sudo apt-get install -y python3-dev python3-tk python-imaging-tk
+sudo apt-get install -y linux-image-generic linux-image-extra-virtual
+sudo apt-get install -y linux-source linux-headers-generic
+
+sudo apt-get purge nvidia*
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt-get update
+
+ubuntu-drivers devices
+
+# Reboot after
+sudo apt-get install nvidia-driver-460
 
